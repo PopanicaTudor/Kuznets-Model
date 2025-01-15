c84100d9b1457830c598b3c623f089660fb0edb1
PopanicaTudor <tudor.popanica@stud.acs.upb.ro>
2025-01-15 11:17:55 +0200

    Credentiale.

--- frontend/src/pages/Credentials.jsx
+++ frontend/src/pages/Credentials.jsx
@@ -4,7 +4,10 @@ import "../styles/Credentials.css";
 const Credentials = () => {
   const emails = [
     { name: "Ion Andrei-Sorin", email: "andrei_sorin.ion@stud.acs.upb.ro" },
-    { name: "Luntraru Maria-Alexandra", email: "maria.luntraru@stud.acs.upb.ro" },
+    {
+      name: "Luntraru Maria-Alexandra",
+      email: "maria.luntraru@stud.acs.upb.ro",
+    },
     { name: "Popanica-Răboj Tudor", email: "tudor.popanica@stud.acs.upb.ro" },
     { name: "Sultzer Roxana-Maria", email: "roxana.sultzer@stud.acs.upb.ro" },
     { name: "Tudor Mihaela", email: "mihaela.tudor2211@stud.acs.upb.ro" },
@@ -27,35 +30,63 @@ const Credentials = () => {
           fontWeight: "bold",
           marginBottom: "20px",
           marginTop: "20px",
-          color: "currentColor", 
+          color: "currentColor",
         }}
       >
         Credentials
       </h1>
 
-      <div style={{ color: "currentColor" }}> {/* Adăugat aici */}
-        <p style={{ lineHeight: "1.6", fontSize: "1em", color: "currentColor" }}> {/* Adăugat aici */}
+      <div style={{ color: "currentColor" }}>
+        {" "}
+        {/* Adăugat aici */}
+        <p
+          style={{ lineHeight: "1.6", fontSize: "1em", color: "currentColor" }}
+        >
+          {" "}
+          {/* Adăugat aici */}
           This project is developed as part of the initiative: <br />
           <br />
-          <strong style={{ color: "currentColor" }}> {/* Adăugat aici */}
-            1. "Gender, Digitalization, Green: Ensuring a Sustainable Future for all in Europe"
-          </strong> <br />
-          Ref. Project: 2023-1-RO01- KA220-HED-000154433,
-          Partnership: Universitatea de Stiinte Agricole si Medicina Veterinara, Bucuresti, Romania, Universitatea Nationala de Stiinta si Tehnologie POLITEHNICA București, Romania, Universitat Autonoma de Barcelona, Espana, Universidade do Porto, República Portuguesa, Uzhgorodskyi Nacionalnyi Universitet, Ukraina.
-
+          <strong style={{ color: "currentColor" }}>
+            {" "}
+            {/* Adăugat aici */}
+            1. "Gender, Digitalization, Green: Ensuring a Sustainable Future for
+            all in Europe"
+          </strong>{" "}
+          <br />
+          Ref. Project: 2023-1-RO01- KA220-HED-000154433, Partnership:
+          Universitatea de Stiinte Agricole si Medicina Veterinara, Bucuresti,
+          Romania, Universitatea Nationala de Stiinta si Tehnologie POLITEHNICA
+          București, Romania, Universitat Autonoma de Barcelona, Espana,
+          Universidade do Porto, República Portuguesa, Uzhgorodskyi Nacionalnyi
+          Universitet, Ukraina.
           <br /> <br />
-          <strong style={{ color: "currentColor" }}> {/* Adăugat aici */}
-            2. "Fostering the Transversal Digital Competences in Higher Education"
-          </strong> <br />
-          Ref. Project: 2022-1-ES01-KA220-HED-000089861, Partnership: Universidad de Malaga, Espana, Universitatea Nationala de Stiinta si Tehnologie POLITEHNICA București, Romania, Univerza na Primorskem Universita del Litorale, Primorskem. Slovenia, Šiauliai State University of Applied Sciences, Šiauliai, Lituania, UC Leuven-Limburg, Belgique
+          <strong style={{ color: "currentColor" }}>
+            {" "}
+            {/* Adăugat aici */}
+            2. "Fostering the Transversal Digital Competences in Higher
+            Education"
+          </strong>{" "}
+          <br />
+          Ref. Project: 2022-1-ES01-KA220-HED-000089861, Partnership:
+          Universidad de Malaga, Espana, Universitatea Nationala de Stiinta si
+          Tehnologie POLITEHNICA București, Romania, Univerza na Primorskem
+          Universita del Litorale, Primorskem. Slovenia, Šiauliai State
+          University of Applied Sciences, Šiauliai, Lituania, UC Leuven-Limburg,
+          Belgique
         </p>
       </div>
 
-      <p style={{ lineHeight: "1.6", fontSize: "1.2em", color: "currentColor" }}> {/* Adăugat aici */}
+      <p
+        style={{ lineHeight: "1.6", fontSize: "1.2em", color: "currentColor" }}
+      >
+        {" "}
+        {/* Adăugat aici */}
         <br /> This project was made by:
       </p>
 
-      <ul style={{ listStyle: "none", padding: 0, color: "currentColor" }}> {/* Adăugat aici */}
+      <ul style={{ listStyle: "none", padding: 0, color: "currentColor" }}>
+        {" "}
+        {/* Adăugat aici */}
         {emails.map((item, index) => (
           <li
             key={index}
@@ -66,10 +97,27 @@ const Credentials = () => {
               color: "currentColor", // Adăugat aici
             }}
           >
-            <strong style={{ color: "currentColor" }}>{item.name}:</strong> {item.email}
+            <strong style={{ color: "currentColor" }}>{item.name}:</strong>{" "}
+            {item.email}
           </li>
         ))}
       </ul>
+
+      <p
+        style={{ lineHeight: "1.6", fontSize: "1.2em", color: "currentColor", marginTop: "20px", marginBottom: "10px" }}  
+      >
+        Project Coordinators:
+      </p>
+      <p
+        style={{ lineHeight: "1.6", fontSize: "1.1em", color: "currentColor" }}
+      >
+        Mihai Caramihai: dan.caramihai@upb.ro
+      </p>
+      <p
+        style={{ lineHeight: "1.6", fontSize: "1.1em", color: "currentColor" }}
+      >
+        Daniel Chiș: daniel_ioan.chis@upb.ro
+      </p>
     </div>
   );
 };
